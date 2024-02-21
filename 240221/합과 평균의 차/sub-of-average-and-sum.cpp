0de69335aa11d73074478ac8b84056153b41b129
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-02-21 17:35:55 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240221/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int sum = a + b + c;
+    int av = sum / 3;
+
+    cout << sum << endl;
+    cout << av << endl;
+    cout << sum - av << endl;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
