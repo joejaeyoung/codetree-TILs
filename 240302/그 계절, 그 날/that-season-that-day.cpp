407672dec4425ff778c414a5e4789cfbaf37d184
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-03-02 21:56:05 +0900

    Title: 그 계절, 그 날, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240302/그 계절, 그 날/that-season-that-day.cpp
+++ 240302/그 계절, 그 날/that-season-that-day.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int get_day[12];
+
 int is_special_year(int y){
     if(y % 4 == 0 && y % 100 && y % 400)
         return 1;
@@ -12,66 +14,58 @@ int is_special_year(int y){
     return 0;
 }
 
+void fill_get_day(){
+    get_day[0] = 31;
+    get_day[1] = -1;
+    get_day[2] = 31;
+    get_day[3] = 30;
+    get_day[4] = 31;
+    get_day[5] = 30;
+    get_day[6] = 31;
+    get_day[7] = 31;
+    get_day[8] = 30;
+    get_day[9] = 31;
+    get_day[10] = 30;
+    get_day[11] = 31;
+}
+
 void deter_season(int y, int m, int d){
     if(m == 2){
-        if(is_special_year(y)){
-            if(d <= 29)
-                cout << "Winter";
-        }else{
-            if(d >= 29)
-                cout << -1;
+        if(is_special_year(y) && d <= 29){
+            cout << "Winter";
+            return ;
         }
+        if(!is_special_year && d <= 28){
+            cout << "Winter";
+            return ;
+        }
+    }
+    if(get_day[m] < d){
+        cout << -1;
         return ;
     }
-
     if(m >= 3 && m <= 5){
-        if(m == 4 && d >= 31){
-            cout << -1;
-            return ;
-        }
-        if(d > 31){
-            cout << -1;
-            return ;
-        }
         cout << "Spring";
         return ;
     }
 
     if(m >= 6 && m <= 8){
-        if(m == 6 && d >= 31){
-            cout << -1;
-            return ;
-        }
-        if(d > 31){
-            cout << -1;
-            return ;
-        }
+
         cout << "Summer";
-        return ;  
+        return ;
     }
 
     if(m >= 9 && m <= 11){
-        if(m == 9 || m == 11&& d >= 31){
-            cout << -1;
-            return ;
-        }
-        if(d > 31){
-            cout << -1;
-            return ;
-        }
+
         cout << "Fall";
-        return ;  
+        return ;
     }
 
-    if(m ==12 && m <= 2){
-        if(d > 31){
-            cout << -1;
-            return ;
-        }
+    if(m == 12 && m <= 2){
+
         cout << "Winter";
-        return ;  
+        return ;
     }
-    cout << -1;
 }
 
 int main() {
