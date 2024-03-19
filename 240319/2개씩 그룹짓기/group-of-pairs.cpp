ced4e6db782217bc88ad39204bb0548b3ec07c47
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-03-19 15:13:11 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/2개씩 그룹짓기/group-of-pairs.cpp
+++ 240319/2개씩 그룹짓기/group-of-pairs.cpp
@@ -8,17 +8,28 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> v(n);
-    for(int i = 0; i < n; i++){
+    vector<int> v(2*n);
+    for(int i = 0; i < (2 * n); i++)
         cin >> v[i];
-    }
+    /*cout << "출력 :" << endl;
+    for(int i = 0; i < 2*n; i++){
+        cout << i << ":" << v[i] <<endl;
+    }*/
 
     sort(v.begin(), v.end());
-
-    int max = 1;
-    for(int i = 0; i <= n / 2; i++){
-        if((v[i] + v[n - i - 1]) / 2 > max)
-            max = v[i] + v[n - i - 1];
+    
+    /*cout << "\n정렬 :\n";
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] <<" ";
+    }
+    cout << endl;
+    */
+    int max = -1;
+    for(int i = 0; i <= n; i++){
+        if((v[i] + v[2*n - 1 - i]) > max){
+            //cout << v[i] << " " << v[2*n - 1 - i] << endl;
+            max = v[i] + v[2*n - 1 - i];
+        }
     }
     cout << max << endl;
     return 0;
