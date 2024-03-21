0ddfbc9b337dd78ad08a8de21f41c73d95aa415f
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-03-21 14:06:06 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int NumOfDay(int m, int d){
+    int monthDays[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total = 0;
+
+    for(int i = 1; i < m; i++){
+        total += monthDays[i];
+    }
+    total += d;
+    return total;
+}
+int main() {
+    string dayList[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    int m1, d1, m2, d2;
+    string day_s;
+    cin >> m1 >> d1 >> m2 >> d2;
+    cin >> day_s;
+    
+    int i = 0;
+    for(i = 0; i < 7; i++){
+        if(day_s.compare(dayList[i]))
+            break;
+    }
+    int diff = NumOfDay(m2, d2) - NumOfDay(m1, d1);
+    int week = diff / 7;
+    int day = diff % 7;
+    if (i <= day)
+        cout << week + 1;
+    else
+        cout << week;
+    return 0;
+}
