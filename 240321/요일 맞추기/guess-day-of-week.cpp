12545f45d28c78919641838052c60d640c40413a
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-03-21 13:58:46 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240321/요일 맞추기/guess-day-of-week.cpp
+++ 240321/요일 맞추기/guess-day-of-week.cpp
@@ -3,25 +3,27 @@
 
 using namespace std;
 
+int NumOfDays(int m, int d) {
+    int days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total_days = 0;
+
+    for(int i = 1; i < m; i++)
+        total_days += days[i];
+
+    total_days += d;
+
+    return total_days;
+}
 int main() {
-    int month_days[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     string days[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
     int m1, d1, m2, d2;
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int count_day = 0;
-    for(int i = m1; i < m2; i++){
-        count_day += month_days[i];
-    }
-    count_day += d2 - d1;
+    int count_day = NumOfDays(m2, d2) - NumOfDays(m1, d1);
 
-    if(count_day <= 0){
-        count_day = count_day % 7;
+    while(count_day < 0)
         count_day += 7;
-        cout << days[count_day % 7];
-    }else{
-        cout << days[count_day % 7];
-    }
 
+    cout << days[count_day % 7];
     return 0;
 }
