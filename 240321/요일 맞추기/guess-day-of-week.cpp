d0ec8df60f972faeae09a50869fc99d4ef4acea8
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-03-21 13:45:10 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240321/요일 맞추기/guess-day-of-week.cpp
+++ 240321/요일 맞추기/guess-day-of-week.cpp
@@ -9,16 +9,16 @@ int main() {
     int m1, d1, m2, d2;
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int cnt = 0;
-    for(int i = m1; i <= m2; i++){
-        for(int j = d1; j <= month_days[i]; j++){
-            if(cnt == 6)
-                cnt = 0;
-            else
-                cnt++;
-        }
+    int count_day = 0;
+    for(int i = m1; i < m2; i++){
+        count_day += month_days[i];
     }
+    count_day += d2 - d1;
 
-    cout << days[cnt % 7];
+    if(count_day < 0){
+        count_day = count_day % 7;
+        count_day += 8;
+    }
+    cout << days[count_day % 7];
     return 0;
 }
