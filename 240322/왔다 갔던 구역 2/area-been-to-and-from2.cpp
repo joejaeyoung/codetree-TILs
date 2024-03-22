889139b4a685abf70a1a28c1da6da6f8063ff25c
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-03-22 16:38:33 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240322/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240322/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -2,12 +2,12 @@
 #include <algorithm>
 #include <string.h>
 
-#define OFFSET 1000
+#define OFFSET 1001
 using namespace std;
 
 int main() {
-    int arr[2001];
-    memset(arr, 0, sizeof(int) * 2001);
+    int arr[2002];
+    memset(arr, 0, sizeof(int) * 2002);
     int n;
 
     int dep;
@@ -17,10 +17,10 @@ int main() {
     while(n--){
         cin >> dep >> dir;
         if(dir == 'R'){
-            for(int i = OFFSET; i < OFFSET + dep; i++)
+            for(int i = OFFSET + 1; i < OFFSET + dep; i++)
                 arr[i]++;
         }else{
-            for(int i = OFFSET; i > OFFSET - dep; i--)
+            for(int i = OFFSET - dep + 1; i < OFFSET; i++)
                 arr[i]++;
         }
     }
@@ -30,6 +30,6 @@ int main() {
         if(arr[i] >= 2)
             cnt++;
     }
-    cout << cnt - 1 << endl;
+    cout << cnt << endl;
     return 0;
 }
