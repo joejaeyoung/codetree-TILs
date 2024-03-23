39e597d794e2e642c13e2678a14ce7406e65dbb1
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-03-23 15:08:02 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240323/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240323/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,44 +1,59 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 
-#define OFFSET 1001
+#define MAX_N 100
+#define MAX_R 2000
+#define OFFSET 1000
 
 using namespace std;
 
-int main() {
-    int arr[2002];
-    memset(arr, 0, sizeof(int) * 2002);
+int n;
+int x1[MAX_N], x2[MAX_N];
 
-    int n;
+int checked[MAX_R + 1];
+
+int main() {
+    // 입력
     cin >> n;
     
-    int dep;
-    char dir;
-    int tmp = OFFSET;
-
-    while(n--){
-        cin >> dep >> dir;
-        if(dir == 'R'){
-            for(int i = tmp; i < tmp + dep; i++){
-                arr[i]++;
-            }
-            tmp += dep;
-        }else{
-            for(int i = tmp; i > tmp - dep; i--){
-                arr[i]++;
-            }
-            tmp -= dep;
-        }
+	// 현재 위치
+	int cur = 0;
+	
+    for(int i = 0; i < n; i++) {
+		int distance;
+		char direction;
+        cin >> distance >> direction;
+		
+		if(direction == 'L') {
+			// 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
+			x1[i] = cur - distance;
+			x2[i] = cur;
+			cur -= distance;
+		}
+		else {
+			// 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
+			x1[i] = cur;
+			x2[i] = cur + distance;
+			cur += distance;
+		}
+        
+        // OFFSET을 더해줍니다.
+        x1[i] += OFFSET;
+        x2[i] += OFFSET;
     }
-
+    
+    // 구간을 칠해줍니다.
+    // 구간 단위로 진행하는 문제이므로
+    // x2[i]에 등호가 들어가지 않음에 유의합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = x1[i]; j < x2[i]; j++)
+            checked[j]++;
+    
+    // 2번 이상 지나간 영역의 크기를 구합니다.
     int cnt = 0;
-    for(int i = 0; i < 2002; i++){
-        if(arr[i] >= 2){
+    for(int i = 0; i <= MAX_R; i++)
+        if(checked[i] >= 2)
             cnt++;
-        }
-    }
-
+    
     cout << cnt;
     return 0;
 }
