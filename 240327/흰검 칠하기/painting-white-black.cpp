f013a07f5fd0a1cf5a44d316076e61940b375f61
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-03-27 16:43:04 +0900

    Title: 흰검 칠하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240327/흰검 칠하기/painting-white-black.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+#define MAX_N 1000
+#define MAX_R 200000
+#define OFFSET 100000
+
+using namespace std;
+
+int n;
+int x1[MAX_N], x2[MAX_N];
+
+int black_checked[MAX_R + 1];
+int white_checked[MAX_R + 1];
+int num[MAX_R + 1];
+
+void print(int left, int right, char dir){
+    left += OFFSET;
+    right += OFFSET;
+
+    if(dir == 'L'){
+        for(int i = left; i < right; i++){
+            white_checked[i]++;
+            num[i] = -1;
+        }
+    }else{
+        for(int i = left; i < right; i++){
+            black_checked[i]++;
+            num[i] = 1;
+        }
+    }
+    //cout << left - OFFSET << " " << right - OFFSET << endl;
+}
+
+int main() {
+    cin >> n;
+    
+	int cur = 0;
+	
+    for(int i = 0; i < n; i++) {
+		int distance;
+		char direction;
+        cin >> distance >> direction;
+		
+		if(direction == 'L') {
+            print(cur - distance, cur, 'L');
+            if(distance == 1)
+			    continue;
+            else
+                cur -= distance;
+		}
+		else {
+            print(cur, cur + distance, 'R');
+			    cur += distance;
+		}
+    }
+    
+    int white = 0;
+    int black = 0;
+    int grey = 0;
+
+    for(int i = 0; i <= MAX_R; i++){
+        if(white_checked[i] >= 2 && black_checked[i] >= 2){
+            //cout << "grey : " << i - OFFSET << endl;
+            grey++;
+        }else{
+            if(num[i] == -1){
+                white++;
+                //cout << "white : " << i - OFFSET << endl;
+            }
+            if(num[i] == 1){
+                black++;
+                //cout << "black : " << i - OFFSET << endl;
+                //cout << black_checked[i] << endl;
+            }
+        }
+    }
+    
+    cout << white << " " << black << " " << grey;
+    return 0;
+}
