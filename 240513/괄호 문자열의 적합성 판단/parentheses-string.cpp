1667f014e5fbeba0d2240908176b3008e08bbb73
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-05-13 17:43:22 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240513/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    stack<char> st;
+
+    string s;
+    cin >> s;
+
+    for(int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '(')
+            st.push(s[i]);
+        if (s[i] == ')')
+            st.pop();
+    }
+    if (st.empty() == true)
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
