b4a76a00b3d8cc106bcb91c04660a25bdf992742
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-07-07 16:15:13 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1081ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240707/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX 101
+
+using namespace std;
+
+int arr[MAX][MAX];
+bool visited[MAX][MAX];
+int result[MAX][MAX];
+int dx[2] = {1, 0};
+int dy[2] = {0, 1};
+bool finish;
+
+int height, width;
+
+bool inRange(int y, int x) {
+    if (x < 0 || x >= width)
+        return false;
+    if (y < 0 || y >= height)
+        return false;
+    return true;
+}
+
+bool CanGo(int y, int x) {
+    if (!inRange(y, x)) {
+        return false;
+    }
+
+    if (arr[y][x] == 0)
+        return false;
+    return true;
+}
+void DFS(int Y, int X) {
+    visited[Y][X] = 1;
+
+    for(int i = 0; i < 2; i++) {
+        int new_x = X + dx[i];
+        int new_y = Y + dy[i];
+        if (CanGo(new_y, new_x))
+            DFS(new_y, new_x);
+    }
+}
+
+int main() {
+    cin >> height >> width;
+
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    DFS(0, 0);
+
+    if (visited[height - 1][width - 1] != 0)
+        cout << "1";
+    else
+        cout << "0";
+    return 0;
+}
