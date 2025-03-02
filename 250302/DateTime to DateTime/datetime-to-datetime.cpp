9fa37dbed69ad88ca12630d47af668599dd1d1cd
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2025-03-02 11:48:51 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250302/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin >> a >> b >> c;
+
+    // Please write your code here.
+    long long firstTime = 11 * 24 * 60 + 11 * 60 + 11;
+    long long endTime = a * 24 * 60 + b * 60 + c;
+    if (endTime <= firstTime)
+        cout << -1;
+
+    cout << endTime - firstTime;
+
+
+    return 0;
+}
