b5db7af9ca752234ca0ebbf221159808ca66f880
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2025-03-02 11:51:26 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250302/DateTime to DateTime/datetime-to-datetime.cpp
+++ 250302/DateTime to DateTime/datetime-to-datetime.cpp
@@ -1,20 +1,40 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int a, b, c;
+
+// int main() {
+//     cin >> a >> b >> c;
+
+//     // Please write your code here.
+//     long long firstTime = 11 * 24 * 60 + 11 * 60 + 11;
+//     long long endTime = a * 24 * 60 + b * 60 + c;
+//     if (endTime < firstTime)
+//         cout << -1;
+
+//     cout << endTime - firstTime;
+
+
+//     return 0;
+// }
+
 #include <iostream>
 
 using namespace std;
 
-int a, b, c;
-
 int main() {
+    // 변수 선언 및 입력
+    int a, b, c;
     cin >> a >> b >> c;
-
-    // Please write your code here.
-    long long firstTime = 11 * 24 * 60 + 11 * 60 + 11;
-    long long endTime = a * 24 * 60 + b * 60 + c;
-    if (endTime < firstTime)
+    
+    // 차이를 계산합니다.
+    int diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11);
+    
+	// 출력
+    if(diff < 0)
         cout << -1;
-
-    cout << endTime - firstTime;
-
-
+    else
+        cout << diff;
     return 0;
 }
