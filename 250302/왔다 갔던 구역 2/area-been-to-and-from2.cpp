a14609933bd354494bbdc0231f16a7f9519b821e
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2025-03-02 19:32:18 +0900

    Title: 왔다 갔던 구역 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250302/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#define OFFSET 1000
+
+using namespace std;
+
+int n;
+int x[100];
+char dir[100];
+int line[3000];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> dir[i];
+    }
+
+    int locate = OFFSET;
+    for(int i = 0; i < n; i++) {
+        if (dir[i] == 'L') {
+            for(int j = 0; j <= x[i] - 1; j++)
+                line[locate--]++;
+        } else {
+            for(int j = 0; j <= x[i] - 1; j++) {
+                line[locate]++;
+                //cout << locate - OFFSET << " ";
+                locate++;
+            }
+        }
+    }
+
+    //cout << "\n";
+
+    int result = 0;
+    for(int i = 0; i < 3000; i++) {
+        if (line[i] >= 2) {
+            result++;
+            //cout << i - OFFSET << " ";
+        }
+    }
+
+    //cout << "\n";
+    cout << result;
+
+    // Please write your code here.
+
+    return 0;
+}
